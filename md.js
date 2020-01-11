05f21c84b199ba53795230527ae1459417f1cebf
vaino-yrjanainen <vaino.yrjanainen@gmail.com>
2020-01-11 16:04:09 +0200

    Implement dynamic language version loading

--- md.js
+++ md.js
@@ -3,7 +3,9 @@ var converter = new showdown.Converter(),
     html      = converter.makeHtml(text);
     //console.log(html);
 
+var supportedLanguages = ["fi", "sv", "fr", "en"];
 
+var defaultLanguage = window.navigator.language
 
 window.onload = function populate() {
 	//document.getElementById("body").innerHTML = html;
@@ -14,6 +16,36 @@ window.onload = function populate() {
 	  html = converter.makeHtml(sampleResp);
 	  document.getElementById("text-container").innerHTML = html;
 	});
+
+	console.log(defaultLanguage);
+
+	var defaultCode = defaultLanguage.substring(0, 2);
+
+	for (x of supportedLanguages) {
+  		if (defaultCode == x) {
+  			console.log("Detect default language:", x)
+  			setLanguage(x);
+  		}
+	} 
 }
 
 
+function setLanguage(language) {
+	//document.getElementById("body").innerHTML = html;
+
+	for (x of supportedLanguages) {
+		document.getElementById(x).classList.remove("activeLang");
+	}
+
+	document.getElementById(language).classList.add("activeLang");
+	//console.log(document.getElementById(language).class);
+	console.log(language);
+	console.log(defaultLanguage);
+
+	Promise.all([
+	  fetch(language + '.md').then(x => x.text()),
+	]).then(([markdown]) => {
+	  html = converter.makeHtml(markdown);
+	  document.getElementById("text-container").innerHTML = html;
+	});
+}
