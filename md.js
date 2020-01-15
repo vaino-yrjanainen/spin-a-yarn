9fe332932be69f81574d16a2a0bddcb0ea4a68c1
vaino-yrjanainen <vaino.yrjanainen@gmail.com>
2020-01-15 13:30:14 +0200

    Refactor .md loading

--- md.js
+++ md.js
@@ -1,34 +1,26 @@
-var converter = new showdown.Converter(),
-    text      = '# hello, markdown!',
-    html      = converter.makeHtml(text);
-    //console.log(html);
+var converter = new showdown.Converter();
 
 var supportedLanguages = ["fi", "sv", "fr", "en"];
-
-var defaultLanguage = window.navigator.language
-
+var defaultLanguage = window.navigator.language;
 
 function populate() {
-	console.log("populate");
-	//document.getElementById("body").innerHTML = html;
-	Promise.all([
-	  fetch('fi.md').then(x => x.text()),
-	]).then(([sampleResp]) => {
-	  console.log(sampleResp);
-	  html = converter.makeHtml(sampleResp);
-	  document.getElementById("text-container").innerHTML = html;
-	});
-
-	console.log(defaultLanguage);
+	console.log("Load page content from .md.");
 
 	var defaultCode = defaultLanguage.substring(0, 2);
 
+	var supported = false
 	for (x of supportedLanguages) {
   		if (defaultCode == x) {
-  			console.log("Detect default language:", x)
+  			console.log("Default browser language supported:", x)
   			setLanguage(x);
+  			supported = true
   		}
-	} 
+	}
+
+	if (!supported) {
+		console.log("Default browser language not supported.", defaultLanguage);
+		setLanguage("en");
+	}
 }
 
 // Load text once DOM has been loaded
@@ -38,21 +30,21 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 function setLanguage(language) {
-	//document.getElementById("body").innerHTML = html;
+	console.log("Set language to: ", language);
 
-	for (x of supportedLanguages) {
-		document.getElementById(x).classList.remove("activeLang");
-	}
-
-	document.getElementById(language).classList.add("activeLang");
-	//console.log(document.getElementById(language).class);
-	console.log(language);
-	console.log(defaultLanguage);
-
+	// Load text from .md, add to DOM
 	Promise.all([
 	  fetch(language + '.md').then(x => x.text()),
 	]).then(([markdown]) => {
 	  html = converter.makeHtml(markdown);
 	  document.getElementById("text-container").innerHTML = html;
 	});
+
+	// Remove underline from previous active language
+	for (x of supportedLanguages) {
+		document.getElementById(x).classList.remove("activeLang");
+	}
+	// Add underline to current active language
+	document.getElementById(language).classList.add("activeLang");
+
 }
